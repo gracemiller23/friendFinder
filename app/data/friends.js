3e687d1f55e5ef96c0ad608959ae807feb54b23f
gracemiller23 <gracemiller0323@gmail.com>
2018-05-24 18:46:51 -0500

    added api and html routes, created server file content

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [
+    {
+        name: "Grace Miller",
+        photo:"https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-0/c0.0.370.370/p370x247/10462606_10152150174191851_6948799341860388495_n.jpg?_nc_cat=0&oh=86548c46272b79f4b78bedfac1ec17b2&oe=5B8C3EA0",
+        scores:[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
+
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,22 +7,10 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+require("./routing/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-//stores data from users
-var friends = [];
 
-//route users to html pages
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "home.html"));
-})
-
-app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "survey.html"));
-})
-
-//route users to api pages
-
-app.get("/api/friends", function(req, res){
-    return res.json(friends);
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
 });
